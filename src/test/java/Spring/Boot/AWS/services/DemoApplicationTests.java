4598831d177ac673556426b5d38832d6e51c31da
Victor-Alexandre09 <victoralexandre.favamp@gmail.com>
2026-08-18 08:48:02 -0300

    first commit

---
+++ src/test/java/Spring/Boot/AWS/services/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Boot.AWS.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
